9e54048a5c4f652c01ecdb8ff5639cd118f59db6
godshen <shenzh0713@gmail.com>
2022-06-02 09:22:09 +0800

    fix 1

--- net/__init__.py
+++ net/__init__.py
@@ -1,4 +1,4 @@
-from weather import get_weather
-from weather import create_ark_obj_list
+from .weather import get_weather
+from .weather import create_ark_obj_list
 
 __all__ = ["get_weather", "create_ark_obj_list"]
