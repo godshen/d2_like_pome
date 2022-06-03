e21a4989fff744147614982796d3789b7e8588c3
godshen <shenzh0713@gmail.com>
2022-06-04 00:11:10 +0800

    pil gen pic

---
+++ pic/main.py
@@ -0,0 +1,25 @@
+import os
+from PIL import ImageFont
+from PIL import Image
+from PIL import ImageDraw
+
+
+if __name__ == "__main__":
+    print("deal picture")
+    # 设置字体，如果没有，也可以不设置
+    font = ImageFont.truetype('./template/simsun.ttf', 60)
+
+    # 打开底版图片
+    imageFile = "./template/pic1.jpg"
+    tp = Image.open(imageFile)
+
+    draw = ImageDraw.Draw(tp)
+    draw.text((600, 330), "累计签到:\t%5d天" % 110, (0, 0, 0), font=font)
+    draw = ImageDraw.Draw(tp)
+    draw.text((600, 430), "连续签到:\t%5d天" % 10, (0, 0, 0), font=font)
+    draw = ImageDraw.Draw(tp)
+    draw.text((600, 530), "目前积分:\t%5d分" % 1120, (0, 0, 0), font=font)
+    draw = ImageDraw.Draw(tp)
+
+    # 保存
+    tp.save("./output/x.png")
