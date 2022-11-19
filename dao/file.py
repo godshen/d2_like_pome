1dd920f6463243cfede8fccfb4a2fdcc8ebab992
godshen <shenzh0713@gmail.com>
2022-11-19 22:21:56 +0800

    from array to map

--- dao/file.py
+++ dao/file.py
@@ -4,14 +4,14 @@ import os
 class RobotFile:
     def __init__(self, draw_one):
         self.draw_one_dir = draw_one
-        self.draw_one_map = []
+        self.draw_one_map = {}
         self.draw_one_len = 0
         self._read_file_map()
 
     def _read_file_map(self):
         file_dir = self.draw_one_dir
         file_lines = open(file_dir).read().splitlines()
-        draw_one_map = []
+        draw_one_map = {}
         for lines in file_lines:
             lines_arr = lines.split(",")
             idx = int(lines_arr[0])
